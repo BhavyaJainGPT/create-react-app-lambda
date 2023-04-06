20c04b6bcca332b99d3a073d8c1c2a6004703d9e
Bhavya <bhavya.jain@gophygital.io>
2023-04-06 19:17:07 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import QrReader from 'react-qr-scanner'
-import React,{useState} from 'react';
+import React,{useRef, useState} from 'react';
 function App() {
   const [data,setData]= useState('');
+
+  const qrReaderRef = useRef(null);
+  const [activeDeviceId,setActiveDeviceId] = useState(null);
+  const [availableDevices,setAvailableDevices] = useState([]);
+ 
+ const handleDeviceChange = (event) => {
+  setActiveDeviceId(event.target.value);
+ }
+
+ const handleDevicesFound = (devices) => {
+  setAvailableDevices(devices);
+  setActiveDeviceId(devices[0]?.deviceId);
+ }
+
   const handleScan = (data) => {
     if(data) {
       console.log(data);
@@ -16,11 +30,20 @@ function App() {
   return (
     <div className="App">
       <div>QR Scanner Web view test</div>
+      <select value={activeDeviceId} onChange={handleDeviceChange}>
+        {availableDevices.map((device)=> (
+          <option key={device.deviceId} value={device.deviceId}>
+            {device.label}
+          </option>
+        ))}
+      </select>
       <div>{data}</div>
      <QrReader
        delay={300}
        video={true}
-       facingMode={"rear"}
+       facingMode={activeDeviceId ? undefined : 'environment'}
+       activeDeviceId={activeDeviceId}
+       onDevicesFound={handleDevicesFound}
        onError={handleError}
        onScan={handleScan}
        style={{width: '100%'}}
