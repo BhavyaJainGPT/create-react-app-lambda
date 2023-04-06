9e7c56256c1b05de0e391ac5a61dcd7f7af0a28c
BhavyaJainGPT <117629752+BhavyaJainGPT@users.noreply.github.com>
2023-04-06 21:15:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import QrReader from 'react-qr-scanner';
 
 const QRScanner = () => {
   const [result, setResult] = useState('');
+  const [facingMode, setFacingMode] = useState('environment');
+  const qrRef = useRef(null);
 
   const handleScan = (data) => {
     if (data) {
@@ -14,14 +16,28 @@ const QRScanner = () => {
     console.error(err);
   }
 
+  const handleSwitchCamera = () => {
+    if (facingMode === 'environment') {
+      setFacingMode('user');
+    } else {
+      setFacingMode('environment');
+    }
+
+    qrRef.current.pause();
+    qrRef.current.play();
+  }
+
   return (
     <div>
       <QrReader
+        ref={qrRef}
         delay={300}
         onError={handleError}
         onScan={handleScan}
         style={{ width: '100%' }}
+        facingMode={facingMode}
       />
+      <button onClick={handleSwitchCamera}>Switch Camera</button>
       <p>{result}</p>
     </div>
   );
