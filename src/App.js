4c85b7d84ccfc16c037ab5a8ca68fe74ba443712
BhavyaJainGPT <117629752+BhavyaJainGPT@users.noreply.github.com>
2023-04-06 20:52:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,40 @@
-import './App.css';
-import QrReader from 'react-qr-scanner'
-import React,{useRef, useState} from 'react';
-function App() {
-  const [data,setData]= useState('');
+import React, { useState, useRef } from "react";
+import QrReader from "react-qr-scanner";
 
-  const qrReaderRef = useRef(null);
-  const [activeDeviceId,setActiveDeviceId] = useState(null);
-  const [availableDevices,setAvailableDevices] = useState([]);
- 
- const handleDeviceChange = (event) => {
-  setActiveDeviceId(event.target.value);
- }
+const QrScanner = () => {
+  const [camera, setCamera] = useState("environment");
+  const videoRef = useRef();
 
- const handleDevicesFound = (devices) => {
-  setAvailableDevices(devices);
-  setActiveDeviceId(devices[0].deviceId);
- }
-
-  const handleScan = (data) => {
-    if(data) {
-      console.log(data);
-      setData(data.text);
+  const handleScan = (result) => {
+    if (result) {
+      alert(result.text);
     }
-  }
-  
+  };
+
   const handleError = (err) => {
-    console.log(err)
-  }
+    console.error(err);
+  };
+
+  const switchCamera = () => {
+    setCamera(camera === "environment" ? "user" : "environment");
+  };
+
   return (
-    <div className="App">
-      <div>QR Scanner Web view test</div>
-      <select value={activeDeviceId} onChange={handleDeviceChange}>
-        {availableDevices.map((device)=> (
-          <option key={device.deviceId} value={device.deviceId}>
-            {device.label}
-          </option>
-        ))}
-      </select>
-      <div>{data}</div>
-     <QrReader
-     ref={qrReaderRef}
-       delay={300}
-       video={true}
-       facingMode={activeDeviceId ? undefined : 'environment'}
-       activeDeviceId={activeDeviceId}
-       onDevicesFound={handleDevicesFound}
-       onError={handleError}
-       onScan={handleScan}
-       style={{width: '100%'}}
-       />
-      
+    <div>
+      <QrReader
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: "100%" }}
+        facingMode={camera}
+        videoConstraints={{
+          facingMode: camera
+        }}
+        ref={videoRef}
+      />
+      <button onClick={switchCamera}>Switch Camera</button>
     </div>
   );
-}
+};
 
-export default App;
+export default QrScanner;
