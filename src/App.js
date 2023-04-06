6c1a67cbc1fb9550ffb65ef025f6aaa8343ddd85
BhavyaJainGPT <117629752+BhavyaJainGPT@users.noreply.github.com>
2023-04-06 21:11:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import { useState, useEffect } from 'react';
-import QrScanner from 'react-qr-scanner';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-scanner';
 
-function App() {
-  const [devices, setDevices] = useState([]);
-  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then((devices) => {
-      setDevices(devices.filter((device) => device.kind === 'videoinput'));
-    });
-  }, []);
+const QRScanner = () => {
+  const [result, setResult] = useState('');
 
   const handleScan = (data) => {
-    console.log(data);
-  };
+    if (data) {
+      setResult(data);
+    }
+  }
 
-  const handleDeviceChange = (event) => {
-    setSelectedDeviceId(event.target.value);
-  };
+  const handleError = (err) => {
+    console.error(err);
+  }
 
   return (
     <div>
-      <select value={selectedDeviceId} onChange={handleDeviceChange}>
-        {devices.map((device) => (
-          <option key={device.deviceId} value={device.deviceId}>
-            {device.label}
-          </option>
-        ))}
-      </select>
-      <QrScanner
+      <QrReader
         delay={300}
-        onError={(err) => console.error(err)}
+        onError={handleError}
         onScan={handleScan}
-        facingMode={null}
-        deviceId={selectedDeviceId}
         style={{ width: '100%' }}
       />
+      <p>{result}</p>
     </div>
   );
 }
-export default App;
+
+export default QRScanner;
