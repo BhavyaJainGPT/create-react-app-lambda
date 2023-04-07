4f7942cc9d0cd3e9e90785c660c5c505a2081cbe
Bhavya <bhavya.jain@gophygital.io>
2023-04-07 09:27:27 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-import './App.css';
-import QrReader from 'react-qr-scanner'
-import React,{useRef, useState} from 'react';
-function App() {
-  const [data,setData]= useState('');
+import "./App.css"
+import { useState } from "react";
+import QrReader from "react-qr-reader";
 
-  const qrReaderRef = useRef(null);
-  const [activeDeviceId,setActiveDeviceId] = useState(null);
-  const [availableDevices,setAvailableDevices] = useState([]);
- 
- const handleDeviceChange = (event) => {
-  setActiveDeviceId(event.target.value);
- }
+const App = () => {
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
 
- const handleDevicesFound = (devices) => {
-  setAvailableDevices(devices);
-  setActiveDeviceId(devices[0].deviceId);
- }
-
-  const handleScan = (data) => {
-    if(data) {
-      console.log(data);
-      setData(data.text);
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
     }
-  }
-  
+  };
   const handleError = (err) => {
-    console.log(err)
-  }
+    console.error(err);
+  };
   return (
     <div className="App">
-      <div>QR Scanner Web view test</div>
-      <select value={activeDeviceId} onChange={handleDeviceChange}>
-        {availableDevices.map((device)=> (
-          <option key={device.deviceId} value={device.deviceId}>
-            {device.label}
-          </option>
-        ))}
-      </select>
-      <div>{data}</div>
-     <QrReader
-     ref={qrReaderRef}
-       delay={300}
-       video={true}
-       facingMode={activeDeviceId ? undefined : 'environment'}
-       activeDeviceId={activeDeviceId}
-       onDevicesFound={handleDevicesFound}
-       onError={handleError}
-       onScan={handleScan}
-       style={{width: '100%'}}
-       />
+     
+    <div>QR Scanner Example</div>
+      <button
+        onClick={() => {
+          setStartScan(!startScan);
+        }}
+      >
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
       
+        <>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Back Camera</option>
+            <option value={"user"}>Front Camera</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
+        </>
+      
+      {loadingScan && <p>Loading</p>}
+      {data !== "" && <p>{data}</p>}
     </div>
   );
-}
+};
 
 export default App;
+
