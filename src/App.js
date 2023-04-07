d9ab8eb82f9e25d0c07bde20c12ca8837fc86f81
Bhavya <bhavya.jain@gophygital.io>
2023-04-07 09:48:37 +0530

    Changes Made

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import React,{useRef, useState} from 'react';
 
 function App() {
   const [data,setData]= useState('');
-  const [selected, setSelected] = useState("environment");
+  const [selected, setSelected] = useState();
   const qrReaderRef = useRef(null);
-  const [activeDeviceId,setActiveDeviceId] = useState(null);
-  const [availableDevices,setAvailableDevices] = useState([]);
  
- const handleDeviceChange = (event) => {
-  setActiveDeviceId(event.target.value);
- }
+ 
 
- const handleDevicesFound = (devices) => {
-   console.log(devices);
-  setAvailableDevices(devices);
-  setActiveDeviceId(devices[0].deviceId);
- }
+
+
 
   const handleScan = (data) => {
     if(data) {
@@ -29,33 +22,32 @@ function App() {
   const handleError = (err) => {
     console.log(err)
   }
+
+  navigator.mediaDevices.enumerateDevices()
+  .then(devices => {
+    const cameras = devices.filter(device => device.kind === 'videoinput')
+    console.log(devices)
+    setSelected(cameras.deviceId)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
   return (
     <div className="App">
       <div>QR Scanner Web view test</div>
-      <select onChange={(e) => setSelected(e.target.value)}>
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select>
-      <select value={activeDeviceId} onChange={handleDeviceChange}>
-        {availableDevices.map((device)=> (
-          <option key={device.deviceId} value={device.deviceId}>
-            {device.label}
-          </option>
-        ))}
-      </select>
+
       <div>{data}</div>
      <QrReader
      ref={qrReaderRef}
        delay={300}
        video={true}
        facingMode={selected}
-       activeDeviceId={activeDeviceId}
-       onDevicesFound={handleDevicesFound}
+       
        onError={handleError}
        onScan={handleScan}
        style={{width: '100%'}}
        />
-      
+      <div>{selected}</div>
     </div>
   );
 }
